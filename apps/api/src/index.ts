ece9a213081e1f4d795c7e8e3ad7b40c9a63b664
Gabe Madonna <gabe@mycopilot.com>
2024-11-11 01:50:54 -0500

    api is functional

---
+++ apps/api/src/index.ts
@@ -0,0 +1,21 @@
+import Fastify from "fastify";
+
+const server = Fastify({ logger: true });
+
+// Define a basic route
+server.get("/ping", async (request, reply) => {
+  return { message: "pong" };
+});
+
+// Start the server
+const start = async () => {
+  try {
+    const address = await server.listen({ port: 0, host: "0.0.0.0" });
+    console.log(`Server is running at ${address}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // root: "./apps/webapp/", // Set this to the directory containing index.html
   plugins: [react()],
-})
+});
